f8159f19eb53162ef9b2271eee88888a2a1c2c65
sanju-27 <45707609+sanju-27@users.noreply.github.com>
2019-01-16 13:54:04 +0530

    Add files via upload

---
+++ fram.py
@@ -0,0 +1,39 @@
+import cv2
+import numpy as np
+import os
+import natsort
+
+from os.path import isfile, join
+
+def convert_frames_to_video(pathIn,pathOut,fps):
+    frame_array = []
+    files = [f for f in os.listdir(pathIn) if isfile(join(pathIn, f))]
+
+    #for sorting the file names properly
+    #files.sort(key = lambda x: int(filter(str.isdigit, x)))
+    files = natsort.natsorted(files)
+    for i in range(len(files)):
+        filename=pathIn + files[i]
+        #reading each files
+        img = cv2.imread(filename)
+        height, width, layers = img.shape
+        size = (width,height)
+        print(filename)
+        #inserting the frames into an image array
+        frame_array.append(img)
+
+    out = cv2.VideoWriter(pathOut,cv2.VideoWriter_fourcc(*'DIVX'), fps, size)
+
+    for i in range(len(frame_array)):
+        # writing to a image array
+        out.write(frame_array[i])
+    out.release()
+
+def main():
+    pathIn= './final1/final0/'
+    pathOut = 'output2.avi'
+    fps = 60.0
+    convert_frames_to_video(pathIn, pathOut, fps)
+
+if __name__=="__main__":
+    main()
---
+++ ftest.py
@@ -0,0 +1,45 @@
+from __future__ import print_function, division
+
+from keras.utils.vis_utils import plot_model
+import new_models as models
+import img_utils
+import os
+os.environ["PATH"] += os.pathsep + 'C:/Program Files (x86)/Graphviz2.38/bin/'
+
+if __name__ == "__main__":
+    path = r"headline_carspeed.jpg"
+    val_path = "val_images/"
+
+    scale = 2 
+
+    """
+    Plot the models
+    """
+
+   
+
+    model = models.ResNetSR(scale).create_model()
+    plot_model(model, to_file="architectures/ResNet.png", show_layer_names=True, show_shapes=True)
+
+   
+    """
+    Train Res Net SR
+    """
+
+    rnsr = models.ResNetSR(scale)
+    rnsr.create_model(load_weights=True)
+    rnsr.fit(nb_epochs=5)
+    model.save('my_model.h5')
+
+    """
+    Evaluate ResNetSR on Set5/14
+    """
+
+    rnsr = models.ResNetSR(scale)
+    rnsr.create_model(None, None, 3, load_weights=True)
+    rnsr.evaluate(val_path)
+
+
+    # rnsr = models.ResNetSR(scale)
+    # rnsr.create_model(None, None, 3, load_weights=True)
+    # rnsr.upscale(path, save_intermediate=False, suffix="rnsr")
